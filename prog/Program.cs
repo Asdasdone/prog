4c39ce2d76b19a33b6618ae302c472553762e9aa
Asdasdone <tohm69@gmail.com>
2020-09-08 11:16:31 +0200

    gép választása

--- prog/Program.cs
+++ prog/Program.cs
@@ -10,7 +10,13 @@ namespace prog
     {
         static void Main(string[] args)
         {
+            string[] tomb = new string[]{ "kő", "papír", "olló" };
+            Random rnd = new Random();
+            string robotval = tomb[rnd.Next(0, 3)];
+            //Console.WriteLine("Gép választása: {0}",robotval);
+            string jatekosv = "";
 
+            Console.ReadKey();
         }
     }
 }
